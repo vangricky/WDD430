592f96b0bc30ad2ba9d9fe0404053affa001fd3a
Ricky Vang <vangcheemeng@yahoo.com>
2025-04-30 18:36:06 -0600

    w02 videos

--- ingredientshopper/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ingredientshopper/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is imply a test', 'https://jamdownfoodie.com/wp-content/uploads/2021/05/IMG_3041-585x585.jpg')
+  ];
 }
