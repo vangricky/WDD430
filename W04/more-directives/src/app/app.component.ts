b9c99180d4581d905b94d3b8e2c1e69ad26802ab
Ricky Vang <vangcheemeng@yahoo.com>
2025-05-15 18:40:04 -0600

    w04

---
+++ W04/more-directives/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'more-directives';
+
+  // numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
+  onlyOdd = false;
+  value = 5;
+}
