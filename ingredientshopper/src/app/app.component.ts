3480402606aa9c29ccace13a6c8d92a058ba5beb
Ricky Vang <vangcheemeng@yahoo.com>
2025-06-09 15:06:31 -0600

    w07 prep

--- ingredientshopper/src/app/app.component.ts
+++ ingredientshopper/src/app/app.component.ts
@@ -3,14 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  loadedFeature = 'recipe';
-
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature
-  }
-
-}
+export class AppComponent {}
