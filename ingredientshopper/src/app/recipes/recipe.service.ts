8a422ddcff9f625ea2a767a0bf8a0c52ceb6ae70
Ricky Vang <vangcheemeng@yahoo.com>
2025-06-30 16:51:48 -0600

    completed w09 tutorial

--- ingredientshopper/src/app/recipes/recipe.service.ts
+++ ingredientshopper/src/app/recipes/recipe.service.ts
@@ -8,22 +8,29 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is imply a test',
-      'https://jamdownfoodie.com/wp-content/uploads/2021/05/IMG_3041-585x585.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-    ),
-    new Recipe(
-      'Another Test Recipe',
-      'This is imply a test',
-      'https://jamdownfoodie.com/wp-content/uploads/2021/05/IMG_3041-585x585.jpg',
-      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'A Test Recipe',
+  //     'This is imply a test',
+  //     'https://jamdownfoodie.com/wp-content/uploads/2021/05/IMG_3041-585x585.jpg',
+  //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     'Another Test Recipe',
+  //     'This is imply a test',
+  //     'https://jamdownfoodie.com/wp-content/uploads/2021/05/IMG_3041-585x585.jpg',
+  //     [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
