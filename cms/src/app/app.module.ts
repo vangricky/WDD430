046c67c20908f80fbfbac19c4bd50e0dcaa2a431
Ricky Vang <vangcheemeng@yahoo.com>
2025-07-01 11:15:22 -0600

    wo9

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { MessageListComponent } from './messages/message-list/message-list.compo
 import { DropdownDirective } from './shared/dropdown.directive';
 import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 import { ContactEditComponent } from './contacts/contact-edit/contact-edit.component';
+import { ContactsFilterPipe } from './contacts/contacts-filter.pipe';
 
 @NgModule({
   declarations: [
@@ -43,6 +44,7 @@ import { ContactEditComponent } from './contacts/contact-edit/contact-edit.compo
     DropdownDirective,
     DocumentEditComponent,
     ContactEditComponent,
+    ContactsFilterPipe,
   ],
   imports: [
     BrowserModule,
